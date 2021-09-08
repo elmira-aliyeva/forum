f84715b14c8553d8cb8549a01525ebba73c3fa13
Elmira Aliyeva <elmira.ali.98@gmail.com>
2021-09-08 14:24:38 -0400

    added forum to my repo

---
+++ cmd/app/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"forum/internal/server"
+
+	_ "github.com/mattn/go-sqlite3" // SQLite driver
+)
+
+func main() {
+	config := server.NewConfig() // generating config for server
+	server := server.New(config) // creating new instance based on the 'config'
+	// Starting the Server
+	if err := server.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
